0b135153fce0f1258be55ae59dd6ffff802efbae
Randy Taylor <randyisbusy@gmail.com>
2019-08-15 16:38:24 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -11,11 +11,10 @@ def self.all
   @@all
 end
 def author=(author_obj)
-  if author_obj.methods.include? :name
+  
     @author = author_obj
     author_obj.posts << self
-  else author_obj.name = "nil"
-  end
+  
 end
 def author_name
   author.name
